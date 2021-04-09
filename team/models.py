2512413e97180806db8f57609edb6af48703b1e5
PeiGiZhu <ZhuXuqi200061@163.com>
2021-04-09 22:57:32 +0800

    team.models修改
    对Contest类增加record_type & record_level
    对Reword类增加record

--- team/models.py
+++ team/models.py
@@ -5,14 +5,27 @@ class Team(models.Model):
     name = models.CharField('队伍名', max_length=30 )
     disable = models.BooleanField('是否存在', default=False)
     users = models.ManyToManyField(User, 'teams')
+    
     class Meta:
         verbose_name = verbose_name_plural = '队伍'
 
 
 class Contest(models.Model):
+    class RewardLevel(models.TextChoices):
+        STATE = 'ST', '国级'
+        PROVINCE = 'PR', '省级'
+        CAMPUS = 'CP', '校级'
+    
+    class RewardType(models.TextChoices):
+        ICPC = 'IC', 'ICPC'
+        CCPC = 'CC', 'CCPC'
+        OTHERS = 'OT', 'Other'
+    
     team_contests = models.ManyToManyField(Team, through='TeamContest', through_fields=('contest', 'team') )
-    name = models.CharField('比赛名称', max_length=30)
-    start_time = models.DateTimeField('比赛时间')
+    name = models.CharField('名称', max_length=30)
+    start_time = models.DateTimeField('时间')
+    record_type = models.CharField('竞赛类型', max_length=2, choices=RewardType.choices)
+    record_level = models.CharField('比赛分级', max_length=2, choices=RewardLevel.choices)
     class Meta:
         verbose_name = verbose_name_plural = '比赛'
 
@@ -25,7 +38,13 @@ class TeamContest(models.Model):
         verbose_name = verbose_name_plural = '队伍-比赛'
 
 class Reward(models.Model):
+    class RewardRanting(models.TextChoices):
+        AU = 'G', '金奖'
+        AG = 'S', '银奖'
+        CU = 'B', '铜奖'
+
     team_contest = models.OneToOneField(TeamContest, on_delete=models.PROTECT)
+    record = models.CharField('奖牌类型', max_length=2, choices=RewardRanting.choices)
     class Meta:
         verbose_name = verbose_name_plural = '奖项'
 
