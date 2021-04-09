5cec1b7b056cb5fc97b39a797674e1e48b05ad83
张钧博 <5525480+mixisama@user.noreply.gitee.com>
2021-04-09 15:17:03 +0800

    第一次提交

---
+++ NWU_ACM_MIS/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for NWU_ACM_MIS project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NWU_ACM_MIS.settings')
+
+application = get_wsgi_application()
