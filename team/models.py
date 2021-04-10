1016a9a69ce29a8cd2c03767b05ed7ec04ef91d9
PeiGiZhu <ZhuXuqi200061@163.com>
2021-04-10 11:56:31 +0800

    修复team
    所有项添加verbose_name
    RewardLever & RewardType改为Contest.... &Contest....
    在部分函数的参数前标明参数类型，
    如:team = models.ForeignKey(Team, models.PROTECT)改为team = models.ForeignKey(to=Team, on_delete=models.PROTECT, verbose_name='队伍')

--- team/models.py
+++ team/models.py
@@ -4,36 +4,36 @@ from user.models import User
 class Team(models.Model):
     name = models.CharField('队伍名', max_length=30 )
     disable = models.BooleanField('是否存在', default=False)
-    users = models.ManyToManyField(User, 'teams')
+    users = models.ManyToManyField(to=User, related_name='teams', verbose_name='队员')
     
     class Meta:
         verbose_name = verbose_name_plural = '队伍'
 
 
 class Contest(models.Model):
-    class RewardLevel(models.TextChoices):
+    class ContestLevel(models.TextChoices):
         STATE = 'ST', '国级'
         PROVINCE = 'PR', '省级'
         CAMPUS = 'CP', '校级'
     
-    class RewardType(models.TextChoices):
+    class ContestType(models.TextChoices):
         ICPC = 'IC', 'ICPC'
         CCPC = 'CC', 'CCPC'
         OTHERS = 'OT', 'Other'
     
-    team_contests = models.ManyToManyField(Team, through='TeamContest', through_fields=('contest', 'team') )
+    team_contests = models.ManyToManyField(to=Team, through='TeamContest', through_fields=('contest', 'team'), verbose_name='队伍-比赛' )
     name = models.CharField('名称', max_length=30)
     start_time = models.DateTimeField('时间')
-    record_type = models.CharField('竞赛类型', max_length=2, choices=RewardType.choices)
-    record_level = models.CharField('比赛分级', max_length=2, choices=RewardLevel.choices)
+    record_type = models.CharField('竞赛类型', max_length=2, choices=ContestType.choices)
+    record_level = models.CharField('比赛分级', max_length=2, choices=ContestLevel.choices)
     class Meta:
         verbose_name = verbose_name_plural = '比赛'
 
 
 class TeamContest(models.Model):
-    team = models.ForeignKey(Team, models.PROTECT)
     have_reward = models.BooleanField('是否获奖', default=False )
-    contest = models.ForeignKey(Contest, models.PROTECT)
+    team = models.ForeignKey(to=Team, on_delete=models.PROTECT, verbose_name='队伍')
+    contest = models.ForeignKey(to=Contest, on_delete=models.PROTECT, verbose_name='比赛')
     class Meta:
         verbose_name = verbose_name_plural = '队伍-比赛'
 
@@ -43,10 +43,9 @@ class Reward(models.Model):
         AG = 'S', '银奖'
         CU = 'B', '铜奖'
 
-    team_contest = models.OneToOneField(TeamContest, on_delete=models.PROTECT)
+    team_contest = models.OneToOneField(to=TeamContest, on_delete=models.PROTECT, verbose_name='队伍-比赛')
     record = models.CharField('奖牌类型', max_length=2, choices=RewardRanting.choices)
     class Meta:
         verbose_name = verbose_name_plural = '奖项'
-
 
 # Create your models here.
