95165fcd2bd9a5ae21175e69ec1350dbcac2303e
NwuLibm <libmght@foxmail.com>
2021-04-10 17:25:21 +0800

    Update views.py

--- person/views.py
+++ person/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from person.serializers import UserSerializer
 from person.models import User
 from django.http import Http404
+from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -16,25 +17,18 @@ class UserList(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-    def get_object(self, pk):
-        try:
-            return User.objects.get(pk=pk)
-        except User.DoesNotExist:
-            raise Http404
-
     def get(self, request, pk=None, format=None):
-        if pk:
-            user = self.get_object(pk)
-            serializer = UserSerializer(user)
-            return Response(serializer.data)
-        else:
+        if pk is None:
             users = User.objects.all()
             serializer = UserSerializer(users, many=True)
-            return Response(serializer.data)
+        else:
+            user = get_object_or_404(User, pk)
+            serializer = UserSerializer(user)
+           
+        return Response(serializer.data)
 
     def put(self, request, pk, format=None):
-        user = self.get_object(pk)
+        user = get_object_or_404(User, pk=pk)
         serializer = UserSerializer(user, data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -42,7 +36,7 @@ class UserList(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk, format=None):
-        user = self.get_object(pk)
+        user = get_object_or_404(User, pk=pk)
         user.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
